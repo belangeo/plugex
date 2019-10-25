4bdf2dbbd990fa5d0df7c3c6de1fb50820e7dca9
Olivier Bélanger <belangeo@gmail.com>
2019-10-24 20:37:29 -0400

    Automatically copy all binaries in a folder on Windows.

--- make_all_win.bat
+++ make_all_win.bat
@@ -1,5 +1,7 @@
 for /f "delims=" %%i in ('dir Plugex_* /b') do (
     cd %%i\Builds\VisualStudio2017
     for %%f in ( dir *.sln ) do ( msbuild %%f -p:Configuration=Release )
+    copy /y /b x64\Release\VST\*.dll ..\..\..\windows_x64_binaries\VST\
+    copy /y /b x64\Release\VST3\*.vst3 ..\..\..\windows_x64_binaries\VST3\
     cd ..\..\..
 )
