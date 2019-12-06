f750c6b2f228bd8f7f36485afa5120660bbe55fd
Olivier Belanger <belangeo@gmail.com>
2019-12-06 12:57:37 -0500

    Added missing M_PI for windows.

--- Plugex_32_SpectralDelay/Source/PluginProcessor.cpp
+++ Plugex_32_SpectralDelay/Source/PluginProcessor.cpp
@@ -12,6 +12,10 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+#ifndef M_PI
+#define M_PI (3.14159265358979323846264338327950288)
+#endif
+
 AudioProcessorValueTreeState::ParameterLayout createParameterLayout() {
     using Parameter = AudioProcessorValueTreeState::Parameter;
 
